1ea09651b373d4d99d5db8d1c6ed30ed9597e8c5
Alasgar910 <44087585+Alasgar910@users.noreply.github.com>
2018-12-17 18:14:47 +0400

    Add files via upload

--- Demo/js/home.js
+++ Demo/js/home.js
@@ -1,1 +1,22 @@
-home.js
+$("#sh").click(function (e) { 
+	e.preventDefault();
+	$("#aa").css("visibility","visible");
+	
+});  
+$("#close").click(function (e) { 
+	e.preventDefault();
+	$("#aa").css("visibility","hidden");
+});
+
+var card= $(".card");
+
+
+card.mouseover(function(){
+	$($(this)[0].children[1]).css("display", "block");
+})
+
+card.mouseout(function(){
+	$($(this)[0].children[1]).css("display", "none");
+})
+
+
